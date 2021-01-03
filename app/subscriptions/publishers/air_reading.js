e5130d4408e96fe8b39628059d03f9024ab5688e
kylehovey <khoveyc@gmail.com>
2021-01-02 23:40:39 -0800

    Set up mock air readings for development

--- app/subscriptions/publishers/air_reading.js
+++ app/subscriptions/publishers/air_reading.js
@@ -3,23 +3,36 @@ const Readline = require('@serialport/parser-readline');
 
 const { pubsub, topics } = require('..');
 
-const serialPath = '/dev/serial/by-id/usb-Arduino_LLC_Arduino_Micro-if00';
-const serialPort = new SerialPort(serialPath, { baudRate: 9600 });
+if (process.env.NODE_ENV === 'production') {
+  const serialPath = '/dev/serial/by-id/usb-Arduino_LLC_Arduino_Micro-if00';
+  const serialPort = new SerialPort(serialPath, { baudRate: 9600 });
 
-const parser = new Readline();
-serialPort.pipe(parser);
+  const parser = new Readline();
+  serialPort.pipe(parser);
 
-parser.on('data', (data) => {
-  const pm25Value = parseInt(data, 10);
+  parser.on('data', (data) => {
+    const pm25Value = parseInt(data, 10);
 
-  if (!Number.isNaN(pm25Value)) {
-    const payload = {
-      pm25: pm25Value,
-    };
+    if (!Number.isNaN(pm25Value)) {
+      const payload = {
+        pm25: pm25Value,
+      };
 
+      pubsub.publish(
+        topics.AIR_READING_TOPIC,
+        { airReading: payload },
+      );
+    }
+  });
+} else {
+  setInterval(() => {
     pubsub.publish(
       topics.AIR_READING_TOPIC,
-      { airReading: payload },
+      {
+        airReading: {
+          pm25: Number.parseInt(Math.random() * 50, 10),
+        },
+      },
     );
-  }
-});
+  }, 1000);
+}
