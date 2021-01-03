e446e69147bf8596fca4422393d4bb4e6e0c32e4
kylehovey <khoveyc@gmail.com>
2021-01-03 00:06:03 -0700

    Removed helmet for now (it broke the react build)

--- app/server.js
+++ app/server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
-const helmet = require('helmet');
 
 const { pubsub, topics } = require('./subscriptions');
 const models = require('./models');
@@ -18,8 +17,6 @@ const apollo = require('./graphql')({
 
 const app = express();
 const server = http.createServer(app);
-
-app.use(helmet());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('tiny'));
