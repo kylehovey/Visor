4ded67e2a6f357f213343cd71d3f1ceab72f33e3
kylehovey <khoveyc@gmail.com>
2021-01-03 13:51:35 -0800

    Moved air quality chart into its own component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,49 +1,15 @@
 import React, { useState } from 'react';
 import { useSubscription, gql } from '@apollo/client';
 
-import Chart from './visualization/chart';
-
-export const GET_AIR_READING = gql`
-  subscription AirReading {
-    airReading {
-      pm25
-    }
-  }
-`;
+import AirChart from './modules/air_quality';
 
 const App = () => {
-  const [ history, setHistory ] = useState([]);
-
-  const {
-    loading: subscriptionLoading,
-    error: subscriptionError,
-  } = useSubscription(GET_AIR_READING, {
-    onSubscriptionData: ({ subscriptionData }) => {
-      const {
-        data: {
-          airReading: {
-            pm25,
-          },
-        },
-      } = subscriptionData;
-
-      setHistory([...history, pm25]);
-    },
-  });
-
-  if (subscriptionLoading) return 'Loading...';
-  if (subscriptionError) return JSON.stringify(subscriptionError);
 
   return (
     <div className="container">
       <div className="content">
-        <div className="main">
-          <div className="description">
-            <h2>Tomahna Environment Statistics</h2>
-          </div>
-          <span>Current Value: {history.slice(-1)[0]}µg/m³</span>
-          <Chart title="Air Quality" values={history} units="µg/m³" />
-        </div>
+        <h2>Tomahna Environment Statistics</h2>
+        <AirChart />
       </div>
     </div>
   );
